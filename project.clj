3f5e1b3d58530e06ae62f55e398993ece06cc2f7
Nikola Vasic <nikola.vasic01@gmail.com>
2024-12-04 08:38:08 +0100

    Quill project start

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject cljinvaders "0.1.0-SNAPSHOT"
+  :description "Simple clojure video game like chicken invaders"
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [quil "4.3.1323"]])
