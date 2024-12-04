f9d967b6a119eba825ecf51d2375203f767d33b0
Nikola Vasic <nikola.vasic01@gmail.com>
2024-12-04 18:25:54 +0100

    Handling key press

--- src/cljinvaders/core.clj
+++ src/cljinvaders/core.clj
@@ -10,14 +10,12 @@
   ; setup function returns initial state. It contains
   ; circle color and position.
   {:color 0
-   :playerX 0
-   :playerY 0})
+   :playerX 500
+   :playerY 900})
 
 (defn update-state [state]
   ; Update sketch state by changing circle color and position.
-  {:color (mod (+ (:color state) 0.7) 255)
-   :playerX (+ (:playerX state) 0.1)
-   :playerY (+ (:playerY state) 0.1)})
+  (assoc state :color (mod (+ (:color state) 0.7) 255)))
 
 (defn draw-state [state]
   ; Clear the sketch by filling it with light-grey color.
@@ -30,17 +28,26 @@
       ; Draw the ellipse at the specified coordinates.
      (q/ellipse x y 100 100)))
 
+(defn handle-key-pressed [state event]
+  (let [key (:key event)]  ; Extract the key from the event
+    (cond
+      (= key :left) (update state :playerX #(max 0 (- % 50)))   ; Left arrow
+      (= key :right) (update state :playerX #(min (q/width) (+ % 50))) ; Right arrow
+      (= key :up) (update state :playerY #(max 0 (- % 50)))     ; Up arrow
+      (= key :down) (update state :playerY #(min (q/height) (+ % 50))) ; Down arrow
+      :else state)))
+
+
 
 (q/defsketch cljinvaders
-  :title "You spin my circle right round"
+  :title "You move my circle right round"
   :size [1000 1000]
   ; setup function called only once, during sketch initialization.
   :setup setup
   ; update-state is called on each iteration before draw-state.
   :update update-state
   :draw draw-state
+  ;; Function for moving
+  :key-pressed handle-key-pressed
   :features [:keep-on-top]
-  ; This sketch uses functional-mode middleware.
-  ; Check quil wiki for more info about middlewares and particularly
-  ; fun-mode.
   :middleware [m/fun-mode])
