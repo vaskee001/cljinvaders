522a27fefe1b5ac5ede7a0b218b0eb21f27cc732
Nikola Vasic <nikola.vasic01@gmail.com>
2024-12-09 14:45:50 +0100

    Random size for asteroids

--- src/cljinvaders/asteroids.clj
+++ src/cljinvaders/asteroids.clj
@@ -2,12 +2,20 @@
 
 (defn create-asteroid [screen-width]
   ; Init asteroid on random place with random speed (between 2 and 3 for now)
-  {:x (rand screen-width)  
-   :y -50                  
-   :speed (+ 2 (rand 3))}) 
+  {:x (rand screen-width)
+   :y -50
+   :size 10
+   :speed (+ 2 (rand 3))})
 
 (defn spawn-asteroids [state]
-  ; There is 2% chance asteroid is spawned (every 50th frame)
-  (if (< (rand) 0.02) 
+  ; There is 1% chance asteroid is spawned (every 100th frame)
+  (if (< (rand) 0.01)
     (update state :asteroids conj (create-asteroid (:screen-width state)))
     state))
+
+(defn update-asteroids [state]
+  (let [moved-asteroids (map (fn [asteroid]
+                               (update asteroid :y #(+ % (:speed asteroid))))
+                             (:asteroids state))
+        visible-asteroids (filter #(<= (:y %) (:screen-height state)) moved-asteroids)]
+    (assoc state :asteroids visible-asteroids)))
