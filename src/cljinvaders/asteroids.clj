af3ddb8ca8a241ad643a68ae76f424534847218b
Nikola Vasic <nikola.vasic01@gmail.com>
2024-12-11 14:44:46 +0100

    Slower shooting

--- src/cljinvaders/asteroids.clj
+++ src/cljinvaders/asteroids.clj
@@ -1,11 +1,24 @@
-(ns cljinvaders.asteroids)
+(ns cljinvaders.asteroids
+  (:require [quil.core :as q]))
+
+(def asteroidImgs (atom []))
+
+(defn random-asteroid-img []
+  (rand-nth @asteroidImgs))
+
+(defn setup-asteroid-images []
+  (reset! asteroidImgs
+          [(q/load-image "src/cljinvaders/img/asteroids/asteroid1.png")
+           (q/load-image "src/cljinvaders/img/asteroids/asteroid2.png")
+           (q/load-image "src/cljinvaders/img/asteroids/asteroid3.png")]))
 
 (defn create-asteroid [screen-width]
   ; Init asteroid on random place with random speed (between 2 and 3 for now)
   {:x (rand screen-width)
    :y -50
    :size (+ 30 (rand 100))
-   :speed (+ 3 (rand 3))})
+   :speed (+ 3 (rand 3))
+   :image (random-asteroid-img)})
 
 (defn spawn-asteroids [state]
   ; There is 1% chance asteroid is spawned (every 100th frame)
