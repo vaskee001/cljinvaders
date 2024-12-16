680a875953841d705a428e884c3336377906302b
Nikola Vasic <nikola.vasic01@gmail.com>
2024-12-16 11:31:46 +0100

    Create asteroid x test

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject cljinvaders "0.1.0-SNAPSHOT"
   :description "Simple clojure video game like chicken invaders"
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [midje "1.10.10"]
-                 [quil "4.3.1323"]]
+                 [quil "4.3.1323"]
+                 [midje "1.10.10"]]
   :plugins [[lein-midje "3.2"]]
   :test-paths ["test"])
 
