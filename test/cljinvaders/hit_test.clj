58034d8df0fc182e8e65c439e6de640f6a48d90f
Nikola Vasic <nikola.vasic01@gmail.com>
2024-12-18 11:41:07 +0100

    Player move projectile test

--- test/cljinvaders/hit_test.clj
+++ test/cljinvaders/hit_test.clj
@@ -18,6 +18,14 @@
                            :asteroids [{:x 0 :y 0 :size 10}]
                            :events []}
                     new-state (hit/handle-hit state hit/on-hit)]
-                ;; There should be any asteroids since it was only one and it is hit
+                ;; There should be any asteroids or projectiles since it was only one and it is hit
                 (and (empty? (get-in new-state [:player :projectiles]))
-                     (empty? (:asteroids new-state)))) => true))
+                     (empty? (:asteroids new-state)))) => true)
+
+        (fact "it removes the projectile and asteroid when they touch"
+              (let [state {:player {:projectiles [{:x 0 :y 0} {:x 30 :y 40}]}
+                           :asteroids [{:x 0 :y 0 :size 10}]
+                           :events []}
+                    new-state (hit/handle-hit state hit/on-hit)]
+                ;; There were two projectiles so one is left
+                (empty? (get-in new-state [:player :projectiles]))) => false))
