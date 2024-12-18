5caed3c0bcbd42a46ca2af0fe7b9fe815c722ea9
Nikola Vasic <nikola.vasic01@gmail.com>
2024-12-18 10:37:51 +0100

    Handle hit test

--- test/cljinvaders/hit_test.clj
+++ test/cljinvaders/hit_test.clj
@@ -6,6 +6,18 @@
        (fact "Classic pythagoras theorem"
              (hit/distance 0 0 3 4) => 5.0))
 
-(facts "aTest hit function"
+(facts "Test hit function"
        (fact "Projectile should hit asteroid"
-             (hit/hit? {:x 0 :y 0} {:x 0 :y 0 :size 10}) => true))
+             (hit/hit? {:x 0 :y 0} {:x 0 :y 0 :size 10}) => true)
+       (fact "Projectile shouldn't hit"
+             (hit/hit? {:x 0 :y 0} {:x 20 :y 20 :size 10}) => false))
+
+(facts "Test handle-hit function"
+        (fact "it removes the projectile and asteroid when they touch"
+              (let [state {:player {:projectiles [{:x 0 :y 0}]}
+                           :asteroids [{:x 0 :y 0 :size 10}]
+                           :events []}
+                    new-state (hit/handle-hit state hit/on-hit)]
+                ;; There should be any asteroids since it was only one and it is hit
+                (and (empty? (get-in new-state [:player :projectiles]))
+                     (empty? (:asteroids new-state)))) => true))
