f9fb299f5f8f9fb38a7e949699a67c3c86f41748
Nikola Vasic <nikola.vasic01@gmail.com>
2025-01-22 11:25:17 +0100

    Scoring system change

--- test/cljinvaders/asteroids_test.clj
+++ test/cljinvaders/asteroids_test.clj
@@ -6,7 +6,7 @@
 (facts "Test create asteroid function"
        (fact "the :x value should not be bigger than screen width + 50"
              (let [asteroid (asteroids/create-asteroid 1920 1080 800)]
-               (and (>= (:x asteroid) 0) (<= (:x asteroid) 1970))) => true)
+               (and (>= (:x asteroid) -50) (<= (:x asteroid) 1970))) => true)
 
        
        (fact "the :y value should always be from -51 to 1131"
@@ -32,20 +32,15 @@
   (fact ":y should be incremented by speed (3)"
         (let [state {:screen-width 1920
                      :screen-height 1080
-                     :asteroids [{:x 100 :y 100 :speed 5}]}
+                     :asteroids [{:x 100 :y 100 :speed 3 :angle (/ Math/PI 2)}]} ; Pi/2 is same angle as falling down
               new-state (asteroids/update-asteroids state)]
           (let [asteroid (first (:asteroids new-state))]
-            (:y asteroid))) => 105)
+            (:y asteroid))) => 103.0)
   (fact "off-screen asteroids should be removed"
         (let [state {:screen-width 1920
                      :screen-height 1080
                      ; Speed added by 5 so 1081 is bigger than 1080 and asteroid is removed
-                     :asteroids [{:x 100 :y 1076 :speed 5}]}
+                     :asteroids [{:x 100 :y 1076 :speed 5 :angle (/ Math/PI 2)}]}
               new-state (asteroids/update-asteroids state)]
           (count (:asteroids new-state))) => 0 ))
-
-
-
-
-
 
